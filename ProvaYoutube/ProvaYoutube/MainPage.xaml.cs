a99006307ad0d2edbb8296b6b6ae819cd6636d10
DanieleLucchetti <daniele.lucchetti@gmail.com>
2017-01-11 22:48:32 +0100

    Prova search

--- ProvaYoutube/ProvaYoutube/MainPage.xaml.cs
+++ ProvaYoutube/ProvaYoutube/MainPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +14,52 @@ namespace ProvaYoutube
     {
         public MainPage()
         {
+            Run();
             InitializeComponent();
+        }
+
+        private async Task Run()
+        {
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyCQ0aGz5ZWuQ_QB1majvXQm4xwc365DQJ0",
+                ApplicationName = this.GetType().ToString()
+            });
+
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = "Google"; // Replace with your search term.
+            searchListRequest.MaxResults = 50;
+
+            // Call the search.list method to retrieve results matching the specified query term.
+            var searchListResponse = await searchListRequest.ExecuteAsync();
+
+            List<string> videos = new List<string>();
+            List<string> channels = new List<string>();
+            List<string> playlists = new List<string>();
+
+            // Add each result to the appropriate list, and then display the lists of
+            // matching videos, channels, and playlists.
+            foreach (var searchResult in searchListResponse.Items)
+            {
+                switch (searchResult.Id.Kind)
+                {
+                    case "youtube#video":
+                        videos.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.VideoId));
+                        break;
+
+                    case "youtube#channel":
+                        channels.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.ChannelId));
+                        break;
+
+                    case "youtube#playlist":
+                        playlists.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.PlaylistId));
+                        break;
+                }
+            }
+
+            Debug.WriteLine(String.Format("Videos:\n{0}\n", string.Join("\n", videos)));
+            Debug.WriteLine(String.Format("Channels:\n{0}\n", string.Join("\n", channels)));
+            Debug.WriteLine(String.Format("Playlists:\n{0}\n", string.Join("\n", playlists)));
         }
     }
 }
